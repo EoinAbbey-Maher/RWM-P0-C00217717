14250278451f8a461d0f12d5eacd6a4d2755a912
Eoin <c002217717@itcarlow.ie>
2019-09-18 13:15:09 +0100

    Project 1

---
+++ RWM-P0-C00217717/Assets/Scripts/CubeFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilter : MonoBehaviour
+{
+   public static int fizzBuzz(int t_int)
+    {
+
+
+        return 0;
+    }
+}
