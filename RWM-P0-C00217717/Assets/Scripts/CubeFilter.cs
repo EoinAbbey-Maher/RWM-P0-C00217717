3d4d29a5f8c3b154f18525b0427dfd440039dd3b
Eoin <c002217717@itcarlow.ie>
2019-09-19 14:52:56 +0100

    script written

--- RWM-P0-C00217717/Assets/Scripts/CubeFilter.cs
+++ RWM-P0-C00217717/Assets/Scripts/CubeFilter.cs
@@ -6,8 +6,32 @@ public class CubeFilter : MonoBehaviour
 {
    public static int fizzBuzz(int t_int)
     {
+        int result = t_int;
+        if (t_int % 3 == 0)
+        {
+            result = 3;
+        }
+        if (t_int % 5 == 0)
+        {
+            result = 5;
+        }
+        if (t_int % 5 == 0 && t_int % 3 == 0)
+        {
+            result = 0;
+        }
 
+        return result;
+    }
 
-        return 0;
+    public static int[] multiFizzBuzz(int[] t_nums)
+    {
+        int[] results = new int[t_nums.Length];
+
+        for(int i = 0;i < t_nums.Length; i++)
+        {
+            results[i] = fizzBuzz(t_nums[i]);   
+        }
+
+        return results;
     }
 }
